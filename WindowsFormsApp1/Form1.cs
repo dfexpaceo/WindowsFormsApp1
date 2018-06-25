8512da82bee8fb0cf2510d7c20b051c6aedcd5a9
Daniel FELICIANO <daniel.feliciano@expaceo.com>
2018-06-25 15:06:48 +0200

    test build avec nugets

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QRCoder;
 
 namespace WindowsFormsApp1
 {
@@ -19,9 +20,23 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("TEST1");
+            //MessageBox.Show("TEST1");
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("The text which should be encoded.", QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            
+
+            this.DrawToBitmap(qrCodeImage, pictureBox1.ClientRectangle);
+            this.BackgroundImage = qrCodeImage;
+            this.Refresh();
+    
         }
 
-
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = null;
+        }
     }
 }
